43adeda41c9b9b62796ea72cc9feb877c2acf9e5
chrysn <chrysn@fsfe.org>
2018-08-21 15:31:59 +0200

    shell updates after hearing that the closure approach won't work

--- src/shell.rs
+++ src/shell.rs
@@ -36,25 +36,21 @@ pub const SHELL_DEFAULT_BUFSIZE: usize = 128;
 // not repr(C) for as long as run() copies over all the inner commands, but there might be a time
 // when we pack it into something null-terminatable from the outside and then repr(C) would help
 // again. -- well actually it seems that we *need* to have an additional slot in here where to
-// store the closure, and don't need to store the full struct.
+// store the closure, and don't need to store the full struct. -- well again not right now, as
+// acting on the closure would need a userdata argument which is not there (cf. freenode/#rust
+// 2018-02-21 14:30CEST), so passing around callbacks directly.
 #[derive(Copy, Clone)]
-pub struct ShellCommand<'a, H: 'a>
+pub struct ShellCommand<'a>
 {
     name: &'a libc::CStr,
     desc: &'a libc::CStr,
-    handler: &'a H,
+    handler: unsafe extern "C" fn(argc: libc::c_int, argv: *mut *mut libc::c_char) -> libc::c_int,
 }
 
-impl<'a, H> ShellCommand<'a, H>
-where H: Fn(&[&str]) -> i32
+impl<'a> ShellCommand<'a>
 {
-    // handler: That one is going to be tricky, as we can't have any allocation next to the
-    // shell_command_handler_t but still need to pack things into a plain C-callable function
-    // pointer.
-    pub fn new(name: &'a libc::CStr, desc: &'a libc::CStr, handler: &'a H) -> Self
+    pub fn new(name: &'a libc::CStr, desc: &'a libc::CStr, handler: unsafe extern "C" fn(argc: libc::c_int, argv: *mut *mut libc::c_char) -> libc::c_int) -> Self
     {
-//         let wrapped_handler = move |argc, argv| handler(&["foo", "bar"]);
-
         ShellCommand {
             name: name,
             desc: desc,
@@ -67,7 +63,7 @@ where H: Fn(&[&str]) -> i32
         shell_command_t {
             name: self.name.as_ptr(),
             desc: self.desc.as_ptr(),
-            handler: Some(failing_handler),
+            handler: Some(self.handler),
         }
     }
 }
@@ -81,13 +77,7 @@ fn null_shell_command() -> shell_command_t
     }
 }
 
-unsafe extern "C" fn failing_handler(argc: i32, argv: *mut *mut i8) -> i32
-{
-    0
-}
-
-pub fn run<H>(command: &ShellCommand<H>, line_buf: &mut[u8]) -> !
-where H: Fn(&[&str]) -> i32
+pub fn run(command: &ShellCommand, line_buf: &mut[u8]) -> !
 {
     const LIMIT: usize = 5;
     // FIXME: Arbitrary size limit, find an idiom to pass in a null-terminated slice or to allocate
@@ -112,4 +102,36 @@ where H: Fn(&[&str]) -> i32
             line_buf.len() as i32, // FIXME: panic if len is too large
             )
     }
+}
+
+/// Take the passed on arguments of a shell_command_handler_t and call an inner function that
+/// receives those arguments in nice str slice form.
+pub fn command_wrap_inner<F>(argc: libc::c_int, argv: *mut *mut libc::c_char, inner: F) -> i32
+where F: Fn(&[&str]) -> i32
+{
+    // Same issue as with run, see LIMIT there
+    const LIMIT: usize = 5;
+    let mut args: [&str; LIMIT] = [&""; 5];
+
+    let argc: usize = if argc < 0 { 0 } else if argc as usize > LIMIT { LIMIT } else { argc as usize };
+
+    let argv: *mut *mut u8 = unsafe { ::core::mem::transmute(argv) };
+    let argv: &[*mut u8] = unsafe { ::core::slice::from_raw_parts(argv, argc) };
+
+    for i in 0..argc {
+        let start = argv[i];
+        // I *really* need a no_std CStr...
+        let mut slice = unsafe { ::core::slice::from_raw_parts(start, 1) };
+        loop {
+            if slice[slice.len() - 1] == 0 {
+                slice = unsafe { ::core::slice::from_raw_parts(start, slice.len() - 1) };
+                break;
+            } else {
+                slice = unsafe { ::core::slice::from_raw_parts(start, slice.len() + 1) };
+            }
+        }
+        args[i] = ::core::str::from_utf8(slice).unwrap();
+    }
+
+    inner(&args[..argc])
 }
