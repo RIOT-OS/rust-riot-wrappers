4ba51b9af33434b4a5f94979297e7506f44f0159
chrysn <chrysn@fsfe.org>
2018-08-23 18:49:56 +0200

    stdio: bypass uartstdio on native boards where libc's facilities are used
    on native, there is no implementation of uartstdio, and rather than that
    providing a backend to libc's stdio system, libc is used unmodified.
    
    as the object in stdio tries to access riot's io facilities directly
    (which might or might not be a good idea), it needs to resort to direct
    c printing (or stdlib, but i'd prefer to avoid that) on native.

--- src/uartstdio.rs
+++ src/uartstdio.rs
@@ -12,6 +12,7 @@ extern "C" {
 pub struct UartStdio {}
 
 impl core::fmt::Write for UartStdio {
+    #[cfg(not(target_os = "linux"))]
     fn write_str(&mut self, s: &str) -> core::fmt::Result
     {
         let data = s.as_bytes();
@@ -23,5 +24,16 @@ impl core::fmt::Write for UartStdio {
         } else {
             Err(core::fmt::Error)
         }
+    }
+
+    // FIXME have a better criterion
+    #[cfg(target_os = "linux")]
+    fn write_str(&mut self, s: &str) -> core::fmt::Result
+    {
+        extern "C" {
+            fn putchar(c: isize) -> isize;
+        }
+        s.as_bytes().iter().for_each(|c| unsafe {putchar(*c as isize);});
+        Ok(())
     }
 }
