6d745710d428f95a8d44f9615dca1bacdb735363
chrysn <chrysn@fsfe.org>
2018-08-30 00:28:56 +0200

    stdio: add read methods

--- src/stdio.rs
+++ src/stdio.rs
@@ -5,6 +5,7 @@ use libc;
 
 use raw::{
     stdio_write,
+    stdio_read,
 };
 
 // Is it OK that everyone can instanciate this at any time just so? Probably yes, because the
@@ -34,5 +35,31 @@ impl core::fmt::Write for Stdio {
         }
         s.as_bytes().iter().for_each(|c| unsafe {putchar(*c as libc::c_int);});
         Ok(())
+    }
+}
+
+impl Stdio {
+    #[cfg(not(target_os = "linux"))]
+    pub fn read_raw<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a mut [u8], ()> {
+        let bytes_read = unsafe { stdio_read(transmute(buffer.as_mut_ptr()), buffer.len()) };
+        if bytes_read >= 0 {
+            Ok(&mut buffer[..bytes_read as usize])
+        } else {
+            Err(())
+        }
+    }
+
+    #[cfg(target_os = "linux")]
+    pub fn read_raw<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a mut [u8], ()> {
+        extern "C" {
+            fn getchar() -> libc::c_int;
+        }
+        // This always reads exactly one character, unlike the UART that often reads many, but
+        // that's not wrong either.
+        let actually_read = unsafe { getchar() };
+
+        let first = &mut buffer[..1];
+        first[0] = actually_read as u8;
+        Ok(first)
     }
 }
