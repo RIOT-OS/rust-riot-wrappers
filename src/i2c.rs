83c1071490428c314c229120822a627c58333491
chrysn <chrysn@fsfe.org>
2018-08-31 10:56:47 +0200

    move native-board workaround implementatiosn into modules
    this makes it easier to keep the warning level down due to unused
    imports.

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,8 +1,10 @@
 use embedded_hal::blocking;
-use libc;
-use raw::{i2c_t, i2c_acquire, i2c_release, i2c_read_bytes, i2c_write_bytes, I2C_COUNT};
+use raw::i2c_t;
 
 pub struct I2CDevice {
+    // because the not_actually_i2c implementation does not use it, but I still want to keep the
+    // signatures the same and re-use initialization.
+    #[allow(dead_code)]
     dev: i2c_t,
 }
 
@@ -23,43 +25,60 @@ pub enum Error {
     DeviceNotFound,
 }
 
-impl blocking::i2c::WriteRead for I2CDevice
-{
-    type Error = Error;
+#[cfg(not(target_os="linux"))]
+mod regular {
+    use super::*;
+    use raw::{i2c_acquire, i2c_release, i2c_read_bytes, i2c_write_bytes, I2C_COUNT};
+    use libc;
 
-    #[cfg(not(target_os="linux"))]
-    fn write_read(&mut self, address: u8, bytes: &[u8], buffer: &mut [u8]) -> Result<(), Self::Error>
+    impl blocking::i2c::WriteRead for I2CDevice
     {
-        // this doesn't really work yet as it doesn't remove the links to i2c_write_bytes (which
-        // are not present in native as no i2c device is here).
-        if I2C_COUNT == 0 {
-            return Err(Error::DeviceNotFound);
-        }
+        type Error = Error;
 
-        let err = unsafe { i2c_acquire(self.dev) };
-        if err != 0 {
-            return Err(Error::AcquireError);
-        }
-        let err = unsafe { i2c_write_bytes(self.dev, address as u16, bytes.as_ptr() as *const libc::c_void, bytes.len(), 0) };
-        if err != 0 {
+        fn write_read(&mut self, address: u8, bytes: &[u8], buffer: &mut [u8]) -> Result<(), Self::Error>
+        {
+            // this doesn't really work yet as it doesn't remove the links to i2c_write_bytes (which
+            // are not present in native as no i2c device is here).
+            if I2C_COUNT == 0 {
+                return Err(Error::DeviceNotFound);
+            }
+
+            let err = unsafe { i2c_acquire(self.dev) };
+            if err != 0 {
+                return Err(Error::AcquireError);
+            }
+            let err = unsafe { i2c_write_bytes(self.dev, address as u16, bytes.as_ptr() as *const libc::c_void, bytes.len(), 0) };
+            if err != 0 {
+                unsafe { i2c_release(self.dev) };
+                return Err(Error::WriteError(err));
+            }
+            let err = unsafe { i2c_read_bytes(self.dev, address as u16, buffer.as_ptr() as *mut libc::c_void, buffer.len(), 0) };
+            if err != 0 {
+                unsafe { i2c_release(self.dev) };
+                return Err(Error::ReadError(err));
+            }
             unsafe { i2c_release(self.dev) };
-            return Err(Error::WriteError(err));
+            Ok(())
         }
-        let err = unsafe { i2c_read_bytes(self.dev, address as u16, buffer.as_ptr() as *mut libc::c_void, buffer.len(), 0) };
-        if err != 0 {
-            unsafe { i2c_release(self.dev) };
-            return Err(Error::ReadError(err));
-        }
-        unsafe { i2c_release(self.dev) };
-        Ok(())
     }
 
-    // for the native board which has no I2C; the discriminator is a terrible choice but works
-    // right now; ideally, the above solution with I2C_COUNT gating would work, but if that's a
-    // dead end, the next best options are pulling some #define-s out into cfg options.
-    #[cfg(target_os="linux")]
-    fn write_read(&mut self, address: u8, bytes: &[u8], buffer: &mut [u8]) -> Result<(), Self::Error>
+}
+
+#[cfg(target_os="linux")]
+mod not_actually_i2c {
+    use super::*;
+
+    impl blocking::i2c::WriteRead for I2CDevice
     {
-        Err(Error::DeviceNotFound)
+        type Error = Error;
+
+        // for the native board which has no I2C; the discriminator is a terrible choice but works
+        // right now; ideally, the above solution with I2C_COUNT gating would work, but if that's a
+        // dead end, the next best options are pulling some #define-s out into cfg options.
+        #[cfg(target_os="linux")]
+        fn write_read(&mut self, _address: u8, _bytes: &[u8], _buffer: &mut [u8]) -> Result<(), Self::Error>
+        {
+            Err(Error::DeviceNotFound)
+        }
     }
 }
--- src/stdio.rs
+++ src/stdio.rs
@@ -1,65 +1,79 @@
-extern crate core;
+#[cfg(not(target_os = "linux"))]
+mod regular {
+    use ::core::intrinsics::transmute;
+    use raw::{
+        stdio_write,
+        stdio_read,
+    };
 
-use core::intrinsics::transmute;
-use libc;
+    // Is it OK that everyone can instanciate this at any time just so? Probably yes, because the
+    // uart_stdio documentation says nothing about limitations on when to call this.
+    pub struct Stdio {}
 
-use raw::{
-    stdio_write,
-    stdio_read,
-};
 
-// Is it OK that everyone can instanciate this at any time just so? Probably yes, because the
-// uart_stdio documentation says nothing about limitations on when to call this.
-pub struct Stdio {}
+    impl ::core::fmt::Write for Stdio {
+        fn write_str(&mut self, s: &str) -> ::core::fmt::Result
+        {
+            let data = s.as_bytes();
+            let result = unsafe { stdio_write(transmute(data.as_ptr()), data.len()) };
 
-impl core::fmt::Write for Stdio {
-    #[cfg(not(target_os = "linux"))]
-    fn write_str(&mut self, s: &str) -> core::fmt::Result
-    {
-        let data = s.as_bytes();
-        let result = unsafe { stdio_write(transmute(data.as_ptr()), data.len()) };
+            if result >= 0 {
+                Ok(())
+            } else {
+                Err(::core::fmt::Error)
+            }
+        }
+    }
 
-        if result >= 0 {
+    impl Stdio {
+        pub fn read_raw<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a mut [u8], ()> {
+            let bytes_read = unsafe { stdio_read(transmute(buffer.as_mut_ptr()), buffer.len()) };
+            if bytes_read >= 0 {
+                Ok(&mut buffer[..bytes_read as usize])
+            } else {
+                Err(())
+            }
+        }
+    }
+}
+
+// FIXME have a better criterion
+#[cfg(target_os = "linux")]
+mod nativestdio {
+    use libc;
+
+    // Is it OK that everyone can instanciate this at any time just so? Probably yes, because the
+    // uart_stdio documentation says nothing about limitations on when to call this.
+    pub struct Stdio {}
+
+    impl ::core::fmt::Write for Stdio {
+        fn write_str(&mut self, s: &str) -> ::core::fmt::Result
+        {
+            extern "C" {
+                fn putchar(c: libc::c_int) -> libc::c_int;
+            }
+            s.as_bytes().iter().for_each(|c| unsafe {putchar(*c as libc::c_int);});
             Ok(())
-        } else {
-            Err(core::fmt::Error)
         }
     }
 
-    // FIXME have a better criterion
-    #[cfg(target_os = "linux")]
-    fn write_str(&mut self, s: &str) -> core::fmt::Result
-    {
-        extern "C" {
-            fn putchar(c: libc::c_int) -> libc::c_int;
+    impl Stdio {
+        pub fn read_raw<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a mut [u8], ()> {
+            extern "C" {
+                fn getchar() -> libc::c_int;
+            }
+            // This always reads exactly one character, unlike the UART that often reads many, but
+            // that's not wrong either.
+            let actually_read = unsafe { getchar() };
+
+            let first = &mut buffer[..1];
+            first[0] = actually_read as u8;
+            Ok(first)
         }
-        s.as_bytes().iter().for_each(|c| unsafe {putchar(*c as libc::c_int);});
-        Ok(())
     }
 }
 
-impl Stdio {
-    #[cfg(not(target_os = "linux"))]
-    pub fn read_raw<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a mut [u8], ()> {
-        let bytes_read = unsafe { stdio_read(transmute(buffer.as_mut_ptr()), buffer.len()) };
-        if bytes_read >= 0 {
-            Ok(&mut buffer[..bytes_read as usize])
-        } else {
-            Err(())
-        }
-    }
-
-    #[cfg(target_os = "linux")]
-    pub fn read_raw<'a>(&mut self, buffer: &'a mut [u8]) -> Result<&'a mut [u8], ()> {
-        extern "C" {
-            fn getchar() -> libc::c_int;
-        }
-        // This always reads exactly one character, unlike the UART that often reads many, but
-        // that's not wrong either.
-        let actually_read = unsafe { getchar() };
-
-        let first = &mut buffer[..1];
-        first[0] = actually_read as u8;
-        Ok(first)
-    }
-}
+#[cfg(not(target_os = "linux"))]
+pub use self::regular::Stdio;
+#[cfg(target_os = "linux")]
+pub use self::nativestdio::Stdio;
