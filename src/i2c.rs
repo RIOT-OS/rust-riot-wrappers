26d925b89d8520ab7b2879c3a21a65c53498dfec
chrysn <chrysn@fsfe.org>
2018-10-03 14:19:02 +0200

    I2C: Implement single operations

--- src/i2c.rs
+++ src/i2c.rs
@@ -73,6 +73,65 @@ mod regular {
         }
     }
 
+    impl blocking::i2c::Write for I2CDevice {
+        type Error = Error;
+
+        fn write(
+            &mut self,
+            address: u8,
+            bytes: &[u8],
+        ) -> Result<(), Self::Error> {
+            let err = unsafe { i2c_acquire(self.dev) };
+            if err != 0 {
+                return Err(Error::AcquireError);
+            }
+            let err = unsafe {
+                i2c_write_bytes(
+                    self.dev,
+                    address as u16,
+                    bytes.as_ptr() as *const libc::c_void,
+                    bytes.len(),
+                    0,
+                )
+            };
+            if err != 0 {
+                unsafe { i2c_release(self.dev) };
+                return Err(Error::WriteError(err));
+            }
+            unsafe { i2c_release(self.dev) };
+            Ok(())
+        }
+    }
+
+    impl blocking::i2c::Read for I2CDevice {
+        type Error = Error;
+
+        fn read(
+            &mut self,
+            address: u8,
+            buffer: &mut [u8],
+        ) -> Result<(), Self::Error> {
+            let err = unsafe { i2c_acquire(self.dev) };
+            if err != 0 {
+                return Err(Error::AcquireError);
+            }
+            let err = unsafe {
+                i2c_read_bytes(
+                    self.dev,
+                    address as u16,
+                    buffer.as_ptr() as *mut libc::c_void,
+                    buffer.len(),
+                    0,
+                )
+            };
+            if err != 0 {
+                unsafe { i2c_release(self.dev) };
+                return Err(Error::ReadError(err));
+            }
+            unsafe { i2c_release(self.dev) };
+            Ok(())
+        }
+    }
 }
 
 #[cfg(not(riot_module_periph_i2c))]
