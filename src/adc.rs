f2cc7c70c20f960a9ac700e113884cb569908165
chrysn <chrysn@fsfe.org>
2018-12-11 21:15:22 +0100

    ADC: overhaul initialization
    The old initialization code was broken in that it just overwrote the
    mutex guarding that hardware ADC.
    
    The new initilization is unsafe, pending making up my mind on how much
    of RIOT's "state which device you use and make sure application-wide you
    only use each once" can go into safe Rust.

--- src/adc.rs
+++ src/adc.rs
@@ -1,4 +1,17 @@
-pub struct ADCLine(pub riot_sys::adc_t);
+pub struct ADCLine(riot_sys::adc_t);
+
+impl ADCLine {
+    /// Initialize an ADC line and get it as a handle. This is declared as unsafe as it may only
+    /// be called once. (A safe abstraction would need to check which RIOT devices have been
+    /// initialized already).
+    pub unsafe fn init(line: riot_sys::adc_t) -> Result<Self, i32> {
+        let success = unsafe { riot_sys::adc_init(line) };
+        match success {
+            0 => Ok(ADCLine(line)),
+            e => Err(success)
+        }
+    }
+}
 
 /// A configured representation of the single operating-system level ADC that RIOT exposes via its
 /// ADC API. The individual ADC lines are addressed as ADCLine structs and can be used uniformly
@@ -16,13 +29,10 @@ impl embedded_hal::adc::Channel<ADC> for ADCLine {
 }
 
 impl embedded_hal::adc::OneShot<ADC, i32, ADCLine> for ADC {
-    type Error = i32;
+    type Error = !;
 
-    fn read(&mut self, pin: &mut ADCLine) -> nb::Result<i32, Self::Error> {
-        let success = unsafe { riot_sys::adc_init(pin.0) };
-        match success {
-            0 => Ok(unsafe { riot_sys::adc_sample(pin.0, self.resolution) }),
-            e => Err(nb::Error::Other(e)),
-        }
+    fn read(&mut self, pin: &mut ADCLine) -> nb::Result<i32, !> {
+        // Sorry, blocking still
+        Ok(unsafe { riot_sys::adc_sample(pin.0, self.resolution) })
     }
 }
