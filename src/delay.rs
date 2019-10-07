263016d0b0415ddf2f228714b26015c6c6ce2eb6
chrysn <chrysn@fsfe.org>
2019-10-07 15:47:47 +0200

    delay: Split up tick calculation and sleeping
    That prepares for
    
    * using xtimer for other things like sleeping, and
    * using more generic duration types (like core::time::Duration) for
      sleeping

--- src/delay.rs
+++ src/delay.rs
@@ -1,29 +1,55 @@
-use core::convert::TryInto;
-
 /// RIOT's global XTimer, which implements a the blocking delay traits to put a thread to sleep for
 /// some time.
 pub struct XTimer {}
 
-impl embedded_hal::blocking::delay::DelayUs<u32> for XTimer {
-    fn delay_us(&mut self, us: u32) {
-        // FIXME: This is a bit like expanded static function, but without all the optimization
-        // EXPANDED sys/include/xtimer/tick_conversion.h:92 (or basically the whole file)
-        let ticks = (us as u64) * (riot_sys::XTIMER_HZ as u64) / 1000000;
-        let ticks = ticks.try_into().expect("Timer overflow");
+impl XTimer {
+    pub fn sleep<T: Into<u64> + Copy>(delay: impl Into<XTimerTicks<T>>) {
+        let ticks = delay.into();
         // EXPANDED sys/include/xtimer/implementation.h:166 (_xtimer_tsleep32)
         // EXPANDED sys/include/xtimer/implementation.h:180 (_xtimer_usleep)
-        unsafe { riot_sys::_xtimer_tsleep(ticks, 0) };
+        unsafe { riot_sys::_xtimer_tsleep(ticks.get_low32(), ticks.get_high32()) };
+    }
+}
+
+impl embedded_hal::blocking::delay::DelayUs<u32> for XTimer {
+    fn delay_us(&mut self, us: u32) {
+        let ticks = XTimerTicks::from_micros_unchecked(us.into());
+        Self::sleep(ticks);
     }
 }
 
 impl embedded_hal::blocking::delay::DelayMs<u32> for XTimer {
     fn delay_ms(&mut self, ms: u32) {
+        let ticks = XTimerTicks::from_millis_unchecked(ms.into());
+        Self::sleep(ticks);
+    }
+}
+
+// Typically, T will be u64 or a smaller unsigned numeric type
+pub struct XTimerTicks<T: Into<u64>>(T);
+
+impl<T: Into<u64> + Copy> XTimerTicks<T> {
+    fn get_low32(&self) -> u32 {
+        self.0.into() as u32
+    }
+
+    fn get_high32(&self) -> u32 {
+        (self.0.into() >> 32) as u32
+    }
+}
+
+impl XTimerTicks<u64> {
+    /// "unchecked" because no wrapping is checked for -- as long as original u32 values are fed
+    /// in, that is probably OK
+    fn from_micros_unchecked(micros: u64) -> Self {
         // FIXME: This is a bit like expanded static function, but without all the optimization
         // EXPANDED sys/include/xtimer/tick_conversion.h:92 (or basically the whole file)
-        let ticks = (ms as u64) * (riot_sys::XTIMER_HZ as u64) / 1000;
-        let ticks = ticks.try_into().expect("Timer overflow");
-        // EXPANDED sys/include/xtimer/implementation.h:166 (_xtimer_tsleep32)
-        // EXPANDED sys/include/xtimer/implementation.h:180 (_xtimer_usleep)
-        unsafe { riot_sys::_xtimer_tsleep(ticks, 0) };
+        Self(micros * (riot_sys::XTIMER_HZ as u64) / 1000000)
+    }
+
+    fn from_millis_unchecked(millis: u64) -> Self {
+        // FIXME: This is a bit like expanded static function, but without all the optimization
+        // EXPANDED sys/include/xtimer/tick_conversion.h:92 (or basically the whole file)
+        Self(millis * (riot_sys::XTIMER_HZ as u64) / 1000)
     }
 }
