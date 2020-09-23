83afd2db73bde0eff5b0d783357bee43883c48ed
chrysn <chrysn@fsfe.org>
2020-09-23 16:57:23 +0200

    SAUL: revamp server side
    The old implementation was unsafe in presence of multiple SAUL users,
    and neither used modern pinning concepts, nor was usable for the general
    case.

--- src/saul.rs
+++ src/saul.rs
@@ -1,4 +1,11 @@
 /// Registration and use of SAUL, the Sensor Actuator Uber Layer
+///
+/// This modules falls largely into two parts:
+///
+/// * `Drivable` and `Registration`, which are used to register custom sensors or actuators into
+///   SAUL, and
+/// * `RegistrationEntry` with its various constructors that find sensors or actuators in SAUL,
+///   which allows interacting with them.
 
 use core::convert::TryFrom;
 
@@ -8,90 +15,102 @@ use riot_sys::libc;
 use crate::error;
 use error::NegativeErrorExt;
 
-/// A struct containing both a registration and its driver
-struct ContainedRegistration<R> {
-    registration: raw::saul_reg_t,
-    driver: raw::saul_driver_t,
-    read: R,
-    // Could probably go into the type information instead, but as long as there's no pinning
-    // available, I'm careful (maybe not even enough) to not tempt Rust to move the code around by
-    // consuming self and returning an almost-but-not-quite identical object on the stack again.
-    registered: bool,
-}
+pub trait Drivable: Sized {
+    /// Read the current state
+    fn read(&self) -> Result<Phydat, ()>;
 
-unsafe extern "C" fn no_writes(_dev: *const libc::c_void, _res: *mut raw::phydat_t) -> i32 {
-    -(raw::ENODEV as i32)
-}
+    /// Set the state of an actuator, or reconfigure a sensor
+    ///
+    /// A &self is passed in on write because there could be concurrent access from multiple SAUL
+    /// users. One option of handling this is to implement Drivable for Mutex<T>.
+    fn write(&self, data: &Phydat) -> Result<(), ()>;
 
-pub trait SimpleSensor {
-    fn start(&mut self) -> Result<(), ()>;
-}
+    /// Sensor class (type)
+    fn class() -> Class;
 
-pub fn create_simple_sensor<R>(name: &libc::CStr, type_: u8, read: R) -> impl SimpleSensor
-where
-    R: FnMut(&mut raw::phydat_t) -> i32,
-{
-    ContainedRegistration {
-        driver: raw::saul_driver_t {
-            read: None,
-            write: Some(no_writes),
-            type_: type_,
-        },
-        registration: raw::saul_reg_t {
-            next: 0 as *mut raw::saul_reg_t,
-            dev: 0 as *mut libc::c_void,
-            name: name.as_ptr(),
-            driver: 0 as *mut raw::saul_driver_t,
-        },
-        read: read,
-        registered: false,
-    }
-}
-
-impl<R> ContainedRegistration<R>
-where
-    R: FnMut(&mut raw::phydat_t) -> i32,
-{
-    // This could do much more in terms of hiding the internals, but phydat_t is sufficiently easy
-    // to use right now.
-    unsafe extern "C" fn run_read(dev: *const libc::c_void, res: *mut raw::phydat_t) -> i32 {
-        let self_ = &mut *(dev as *mut ContainedRegistration<R>);
-        let res = &mut *res;
-        let f = &mut self_.read;
-        f(res)
-    }
-}
-
-impl<R> SimpleSensor for ContainedRegistration<R>
-where
-    R: FnMut(&mut raw::phydat_t) -> i32,
-{
-    /// Do everything that could not be done in the constructor but needs the result to stay pinned
-    /// -- even if it isn't officially expressed through any Pin marker, this hopefully doesn't
-    /// move any more.
-    fn start(&mut self) -> Result<(), ()> {
-        self.registration.driver = &self.driver;
-        self.registration.dev = self as *mut Self as *mut libc::c_void;
-        self.driver.read = Some(ContainedRegistration::<R>::run_read);
-
-        let success = unsafe { raw::saul_reg_add(&mut self.registration) };
-        match success {
-            0 => {
-                self.registered = true;
-                Ok(())
+    unsafe extern "C" fn read_raw(dev: *const libc::c_void, res: *mut raw::phydat_t) -> i32 {
+        let device = &*(dev as *const Self);
+        match device.read() {
+            Ok(d) => {
+                res.write(d.values);
+                d.length.into()
             }
-            _ => Err(()),
+            // The only legal device error -- ENOTSUP would mean there's no handler at all
+            Err(_) => -(riot_sys::ECANCELED as i32),
+        }
+    }
+
+    unsafe extern "C" fn write_raw(dev: *const libc::c_void, data: *mut raw::phydat_t) -> i32 {
+        let device = &*(dev as *const Self);
+        let data = *data;
+        // FIXME: Should distinguiwh between lengthful and lengthless Phydat
+        let data = Phydat { values: data, length: 3 };
+        match Self::write(device, &data) {
+            // FIXME let the writer return that
+            Ok(()) => data.length.into(),
+            // The only legal device error -- ENOTSUP would mean there's no handler at all
+            Err(_) => -(riot_sys::ECANCELED as i32),
+        }
+    }
+
+    fn build_driver() -> Driver<Self> {
+        Driver {
+            driver: riot_sys::saul_driver_t {
+                // FIXME: Allow the device not to implement one of them and then put None here
+                read: Some(Self::read_raw),
+                write: Some(Self::write_raw),
+                type_: Self::class().to_c(),
+            },
+            _phantom: core::marker::PhantomData,
         }
     }
 }
 
-impl<R> Drop for ContainedRegistration<R> {
-    // should maybe already trigger on moves, and re-register after the moves?
-    fn drop(&mut self) {
-        if self.registered {
-            // Not evaluating the error code, for what could we do...
-            unsafe { raw::saul_reg_rm(&mut self.registration) };
+/// A typed saul_driver_t, created from a Drivable's build_driver() static method, and used in
+/// pinned form in registrations.
+pub struct Driver<D: Drivable> {
+    driver: riot_sys::saul_driver_t,
+    _phantom: core::marker::PhantomData<D>,
+}
+
+// The 'a lifetime is only formal -- as for registration a Pin<&Self> is required and the
+// destructor blocks, the practically 'a is static (but the compiler can't know that by the time
+// it's checking).
+pub struct Registration<'a, D: Drivable> {
+    reg: riot_sys::saul_reg_t,
+    _phantom: core::marker::PhantomData<&'a D>,
+}
+
+impl<'a, D: Drivable> Registration<'a, D> {
+    pub fn new(
+        driver: &Driver<D>,
+        device: &D,
+        name: Option<&libc::CStr>
+    ) -> Self
+    {
+        Registration {
+            reg: riot_sys::saul_reg_t {
+                next: 0 as _,
+                dev: device as *const _ as *mut _,
+                name: name.map(|n| n.as_ptr()).unwrap_or(0 as _),
+                driver: &driver.driver as *const _,
+            },
+            _phantom: core::marker::PhantomData,
         }
+    }
+
+    // The signature ensures that the registration will always be around under penalty of a
+    // panicking drop, not moved, and that register is only called once.
+    pub fn register(self: core::pin::Pin<&mut Self>) {
+        (unsafe { riot_sys::saul_reg_add(&mut self.get_unchecked_mut().reg) })
+            .negative_to_error()
+            .expect("Constructed registries are always valid");
+    }
+}
+
+impl<'a, D: Drivable> Drop for Registration<'a, D> {
+    fn drop(&mut self) {
+        panic!("A SAUL registration must persist for the complete device uptime.")
     }
 }
 
@@ -141,12 +160,13 @@ impl RegistryEntry {
     }
 
     /// Read a value from the SAUL device
-    pub fn read(&self) -> Phydat {
+    pub fn read(&self) -> Result<Phydat, error::NumericError> {
         // Could work with MaybeUninit here, but probably not worth it.
         let mut result: Phydat = Default::default();
-        let length = unsafe { riot_sys::saul_reg_read(self.0 as *mut _, &mut result.values) };
+        let length = (unsafe { riot_sys::saul_reg_read(self.0 as *mut _, &mut result.values) })
+            .negative_to_error()?;
         result.length = length as _;
-        result
+        Ok(result)
     }
 
     /// Write a value to the SAUL device
@@ -255,6 +275,50 @@ impl Class {
             x if x & riot_sys::SAUL_CAT_MASK == riot_sys::SAUL_CAT_SENSE => Some(Sensor(None)),
             _ => None
         }
+    }
+
+    fn to_c(self) -> u8 {
+        use Class::*;
+        use ActuatorClass::*;
+        use SensorClass::*;
+
+        (match self {
+            Actuator(None) => riot_sys::SAUL_ACT_ANY,
+            Actuator(Some(LedRgb)) => riot_sys::SAUL_ACT_LED_RGB,
+            Actuator(Some(Servo)) => riot_sys::SAUL_ACT_SERVO,
+            Actuator(Some(Motor)) => riot_sys::SAUL_ACT_MOTOR,
+            Actuator(Some(Switch)) => riot_sys::SAUL_ACT_SWITCH,
+            Actuator(Some(Dimmer)) => riot_sys::SAUL_ACT_DIMMER,
+            Sensor(None) => riot_sys::SAUL_SENSE_ANY,
+            Sensor(Some(Btn)) => riot_sys::SAUL_SENSE_BTN,
+            Sensor(Some(Temp)) => riot_sys::SAUL_SENSE_TEMP,
+            Sensor(Some(Hum)) => riot_sys::SAUL_SENSE_HUM,
+            Sensor(Some(Light)) => riot_sys::SAUL_SENSE_LIGHT,
+            Sensor(Some(Accel)) => riot_sys::SAUL_SENSE_ACCEL,
+            Sensor(Some(Mag)) => riot_sys::SAUL_SENSE_MAG,
+            Sensor(Some(Gyro)) => riot_sys::SAUL_SENSE_GYRO,
+            Sensor(Some(Color)) => riot_sys::SAUL_SENSE_COLOR,
+            Sensor(Some(Press)) => riot_sys::SAUL_SENSE_PRESS,
+            Sensor(Some(Analog)) => riot_sys::SAUL_SENSE_ANALOG,
+            Sensor(Some(Uv)) => riot_sys::SAUL_SENSE_UV,
+            Sensor(Some(Objtemp)) => riot_sys::SAUL_SENSE_OBJTEMP,
+            Sensor(Some(Count)) => riot_sys::SAUL_SENSE_COUNT,
+            Sensor(Some(Distance)) => riot_sys::SAUL_SENSE_DISTANCE,
+            Sensor(Some(Co2)) => riot_sys::SAUL_SENSE_CO2,
+            Sensor(Some(Tvoc)) => riot_sys::SAUL_SENSE_TVOC,
+            Sensor(Some(Gas)) => riot_sys::SAUL_SENSE_GAS,
+            Sensor(Some(Occup)) => riot_sys::SAUL_SENSE_OCCUP,
+            Sensor(Some(Proximity)) => riot_sys::SAUL_SENSE_PROXIMITY,
+            Sensor(Some(Rssi)) => riot_sys::SAUL_SENSE_RSSI,
+            Sensor(Some(Charge)) => riot_sys::SAUL_SENSE_CHARGE,
+            Sensor(Some(Current)) => riot_sys::SAUL_SENSE_CURRENT,
+            Sensor(Some(Pm)) => riot_sys::SAUL_SENSE_PM,
+            Sensor(Some(Capacitance)) => riot_sys::SAUL_SENSE_CAPACITANCE,
+            Sensor(Some(Voltage)) => riot_sys::SAUL_SENSE_VOLTAGE,
+            Sensor(Some(Ph)) => riot_sys::SAUL_SENSE_PH,
+            Sensor(Some(Power)) => riot_sys::SAUL_SENSE_POWER,
+            Sensor(Some(Size)) => riot_sys::SAUL_SENSE_SIZE,
+        }) as _
     }
 }
 
