b602af8305873e8aded556632405cfd5fb9f5a67
chrysn <chrysn@fsfe.org>
2021-03-26 21:35:20 +0100

    GPIO: Add InOut mode, make pins configurable
    This deprecates the previous not-fully-thought-through plain conversion
    interface.

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,10 +1,61 @@
-use riot_sys::{gpio_clear, gpio_read, gpio_set, gpio_t, gpio_toggle};
+use riot_sys::{gpio_clear, gpio_read, gpio_set, gpio_t, gpio_toggle, gpio_mode_t};
 
 use embedded_hal::digital::v2::{InputPin, OutputPin, ToggleableOutputPin};
+
+use crate::error::NegativeErrorExt;
 
 /// A Rust representation of RIOT's gpio_t, representing a single pin in no particular
 /// configuration.
 pub struct GPIO(gpio_t);
+
+#[non_exhaustive]
+pub enum InputMode {
+    In,
+    InPullDown,
+    InPullUp,
+}
+
+impl InputMode {
+    fn to_c(self) -> gpio_mode_t {
+        match self {
+            InputMode::In => riot_sys::gpio_mode_t_GPIO_IN,
+            InputMode::InPullDown => riot_sys::gpio_mode_t_GPIO_IN_PD,
+            InputMode::InPullUp => riot_sys::gpio_mode_t_GPIO_IN_PU,
+        }
+    }
+}
+
+#[non_exhaustive]
+pub enum OutputMode {
+    Out,
+    OpenDrain,
+    OpenDrainPullUp,
+}
+
+impl OutputMode {
+    fn to_c(self) -> gpio_mode_t {
+        match self {
+            OutputMode::Out => riot_sys::gpio_mode_t_GPIO_OUT,
+            OutputMode::OpenDrain => riot_sys::gpio_mode_t_GPIO_OD,
+            OutputMode::OpenDrainPullUp => riot_sys::gpio_mode_t_GPIO_OD_PU,
+        }
+    }
+}
+
+#[non_exhaustive]
+pub enum InOutMode {
+    OpenDrain,
+    OpenDrainPullUp,
+}
+
+impl InOutMode {
+    fn to_c(self) -> gpio_mode_t {
+        match self {
+            InOutMode::OpenDrain => riot_sys::gpio_mode_t_GPIO_OD,
+            InOutMode::OpenDrainPullUp => riot_sys::gpio_mode_t_GPIO_OD_PU,
+        }
+    }
+}
 
 impl GPIO {
     /// Create a GPIO from a gpio_t
@@ -12,6 +63,11 @@ impl GPIO {
     /// This is as safe as any device acquisition from C is -- RIOT's drivers are (hopefully)
     /// written in such a way that concurrent writes to adjacent pins don't interfere, and those to
     /// the same device are "just" racy.
+    ///
+    /// (This also means that it is completely possible to have two objects for the same pin
+    /// configured in different states, changing the mode while the other is around. The underlying
+    /// operating system operates this, but interactions with a reconfigured pin will obviously not
+    /// have the intended effect).
     pub fn from_c(gpio: gpio_t) -> Option<Self> {
         if unsafe { riot_sys::gpio_is_valid(gpio) } != 0 {
             Some(GPIO(gpio))
@@ -29,14 +85,34 @@ impl GPIO {
     //         GPIO(((port << 5) | pin).into())
     //     }
 
+    #[deprecated(note="Use configure_as_output")]
     pub unsafe fn as_output(self) -> OutputGPIO {
         // FIXME should we configure here? it's probably even safe
         OutputGPIO(self)
     }
 
+    #[deprecated(note="Use configure_as_input")]
     pub unsafe fn as_input(self) -> InputGPIO {
         // FIXME should we configure here? it's probably even safe
         InputGPIO(self)
+    }
+
+    pub fn configure_as_output(self, mode: OutputMode) -> Result<OutputGPIO, crate::error::NumericError> {
+        unsafe { riot_sys::gpio_init(self.0, mode.to_c()) }
+            .negative_to_error()?;
+        Ok(OutputGPIO(self))
+    }
+
+    pub fn configure_as_input(self, mode: InputMode) -> Result<InputGPIO, crate::error::NumericError> {
+        unsafe { riot_sys::gpio_init(self.0, mode.to_c()) }
+            .negative_to_error()?;
+        Ok(InputGPIO(self))
+    }
+
+    pub fn configure_as_inout(self, mode: InOutMode) -> Result<InOutGPIO, crate::error::NumericError> {
+        unsafe { riot_sys::gpio_init(self.0, mode.to_c()) }
+            .negative_to_error()?;
+        Ok(InOutGPIO(self))
     }
 
     /// Get a gpio_t from a configured pin
@@ -54,6 +130,11 @@ impl OutputGPIO {
     /// See [GPIO::to_c]
     pub fn to_c(&self) -> riot_sys::gpio_t {
         self.0.to_c()
+    }
+
+    /// Lose information about how the pin is configured, making it configurable again
+    pub fn deconfigured(self) -> GPIO {
+        self.0
     }
 }
 
@@ -87,6 +168,11 @@ impl InputGPIO {
     pub fn to_c(&self) -> riot_sys::gpio_t {
         self.0.to_c()
     }
+
+    /// Lose information about how the pin is configured, making it configurable again
+    pub fn deconfigured(self) -> GPIO {
+        self.0
+    }
 }
 
 impl InputPin for InputGPIO {
@@ -98,5 +184,54 @@ impl InputPin for InputGPIO {
 
     fn is_low(&self) -> Result<bool, !> {
         Ok(unsafe { gpio_read(self.to_c()) } == 0)
+    }
+}
+
+pub struct InOutGPIO(GPIO);
+
+impl InOutGPIO {
+    /// See [GPIO::to_c]
+    pub fn to_c(&self) -> riot_sys::gpio_t {
+        self.0.to_c()
+    }
+
+    /// Lose information about how the pin is configured, making it configurable again
+    pub fn deconfigured(self) -> GPIO {
+        self.0
+    }
+}
+
+impl InputPin for InOutGPIO {
+    type Error = !;
+
+    fn is_high(&self) -> Result<bool, !> {
+        Ok(unsafe { gpio_read(self.to_c()) } != 0)
+    }
+
+    fn is_low(&self) -> Result<bool, !> {
+        Ok(unsafe { gpio_read(self.to_c()) } == 0)
+    }
+}
+
+impl OutputPin for InOutGPIO {
+    type Error = !;
+
+    fn set_high(&mut self) -> Result<(), !> {
+        unsafe { gpio_set(self.to_c()) };
+        Ok(())
+    }
+
+    fn set_low(&mut self) -> Result<(), !> {
+        unsafe { gpio_clear(self.to_c()) };
+        Ok(())
+    }
+}
+
+impl ToggleableOutputPin for InOutGPIO {
+    type Error = !;
+
+    fn toggle(&mut self) -> Result<(), !> {
+        unsafe { gpio_toggle(self.to_c()) };
+        Ok(())
     }
 }
