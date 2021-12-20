555a25f7d5fd1bddd136c3abb0c5dd9f58a31808
chrysn <chrysn@fsfe.org>
2021-12-20 10:35:57 +0100

    ztimer: Add API that uses ztimer_remove return values

--- src/ztimer.rs
+++ src/ztimer.rs
@@ -49,6 +49,77 @@ impl<const HZ: u32> ZTimer<HZ> {
         }
         self.sleep_ticks(ticks.try_into().expect("Was just checked manually above"));
     }
+
+    /// Set the given callback to be executed in an interrupt some ticks in the future.
+    ///
+    /// Then, start the in_thread function from in the thread this is called from (as a regular
+    /// function call).
+    ///
+    /// After the in_thread function terminates, the callback is dropped if it has not already
+    /// triggered.
+    ///
+    /// Further Development:
+    ///
+    /// * This could probably be done with some sort of pinning instead, thus avoiding the nested
+    ///   scope -- but getting the Drop right is comparatively tricky, because when done naively it
+    ///   needs runtime state.
+    ///
+    /// * The callback could be passed something extra that enables it to set the timer again and
+    ///   again. Granted, there's ztimer_periodic for these cases (and it has better drifting
+    ///   properties), but for something like exponential retransmission it could be convenient.
+    ///
+    ///   (Might make sense to do this without an extra function variant: if the callback ignores
+    ///   the timer argument and always returns None, that's all in the caller type and probebly
+    ///   inlined right away).
+    pub fn set_ticks_during<I: FnOnce() + Send, M: FnOnce() -> R, R>(
+        &self,
+        callback: I,
+        ticks: u32,
+        in_thread: M,
+    ) -> R {
+        use core::mem::ManuallyDrop;
+
+        // This is zero-initialized, which is the more efficient mode for ztimer_t.
+        let mut timer = riot_sys::ztimer_t::default();
+
+        // FIXME: If we were worried about what this does during unwind, we might put a Drop on a
+        // type around this. (But currenlty, Rust on RIOT does not unwind).
+        //
+        // As this is later put into timer.arg, this will need to stay put now (but we can't
+        // directly Pin<&mut> it because we need ownership for the FnOnce)
+        let mut callback = ManuallyDrop::new(callback);
+
+        extern "C" fn caller<I: FnOnce() + Send>(arg: *mut riot_sys::libc::c_void) {
+            // unsafe: Was cast from the same type when assigned to arg.
+            let callback: &mut ManuallyDrop<I> = unsafe { &mut *(arg as *mut ManuallyDrop<I>) };
+            // unsafe: The other take (actually drop) coordinates through the ztimer return value,
+            // so that only one of these is ever run.
+            let taken = unsafe { ManuallyDrop::take(callback) };
+            taken();
+        }
+
+        timer.callback = Some(caller::<I>);
+        timer.arg = &mut callback as *mut _ as *mut _;
+
+        // unsafe: OK per C API
+        unsafe {
+            riot_sys::ztimer_set(self.0, &mut timer, ticks);
+        }
+
+        let result = in_thread();
+
+        // unsafe: OK per C API
+        let removed = unsafe { riot_sys::ztimer_remove(self.0, &mut timer) };
+
+        if removed {
+            // unsafe: removed == true means that the other drop (actually take) has not been run
+            unsafe {
+                ManuallyDrop::drop(&mut callback);
+            }
+        }
+
+        result
+    }
 }
 impl ZTimer<1> {
     /// Get the global second ZTimer, ZTIMER_SEC.
