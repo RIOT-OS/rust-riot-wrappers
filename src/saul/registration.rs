fbef6cf902ba23b65824852eae2835e22066b099
chrysn <chrysn@fsfe.org>
2022-01-25 12:03:44 +0100

    saul: Allow drivers to be split
    The usual single-driver devices can still almost work as they always did
    (the impl is now on &T and takes self; the retained property is that no
    extra type needs to be introduced for a single-driver device), but
    Drivable can also be implemented on some Aspect type (typically `struct
    Aspect<&'static Device>`), for which Drivable is then impleneted in
    addition.

--- src/saul/registration.rs
+++ src/saul/registration.rs
@@ -21,9 +21,12 @@ use crate::Never;
 /// the `HAS_{READ,WRITE}` consts).
 pub struct Error;
 
-// Sync is required because callers from any thread may use the raw methods to construct a self
-// reference through which it is used
-pub trait Drivable: Sized + Sync + 'static {
+/// API through which SAUL operations are done
+///
+/// This is typically implemented on a `&T` (where T is what the Driver and Registration is for),
+/// but can be alternatively implemented on a newtype around such pointers to drive various aspects
+/// of a device.
+pub trait Drivable: Sized {
     /// Sensor class (type)
     const CLASS: Class;
 
@@ -37,7 +40,7 @@ pub trait Drivable: Sized + Sync + 'static {
     const HAS_WRITE: bool = false;
 
     /// Read the current state
-    fn read(&self) -> Result<Phydat, Error> {
+    fn read(self) -> Result<Phydat, Error> {
         // This function's presence in generated code should already show that something is
         // configured badly; could consider making that a linker error (but riot-wrappers is not in
         // the habit of doing that).
@@ -54,7 +57,7 @@ pub trait Drivable: Sized + Sync + 'static {
     /// (which contains a length) with the maximum available length (some of which may contain
     /// uninitialized data, which is OK as i16 has no uninhabited values), and the writer needs to
     /// return how many of the entries it actually used.
-    fn write(&self, _data: &Phydat) -> Result<u8, Error> {
+    fn write(self, _data: &Phydat) -> Result<u8, Error> {
         // See also comment in read()
         unimplemented!("Sensor writing not implemented; HAS_READ should not have been set.")
     }
@@ -62,33 +65,48 @@ pub trait Drivable: Sized + Sync + 'static {
 
 /// A typed saul_driver_t, created from a Drivable's build_driver() static method, and used as
 /// statically lived references registrations.
-pub struct Driver<D: Drivable> {
+///
+/// `DEV` indicates the type of the item pointed to in the registration's field, which is usually the
+/// Drivable itself, but may be specialized by AsRef into a particular drivable, eg. when a device
+/// is used by two drivers representing different aspects of the device.
+pub struct Driver<DEV, DRIV = &'static DEV>
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
     driver: riot_sys::saul_driver_t,
-    _phantom: core::marker::PhantomData<D>,
+    _phantom: core::marker::PhantomData<(DEV, DRIV)>,
 }
 
-impl<D: Drivable> Driver<D> {
+impl<DEV, DRIV> Driver<DEV, DRIV>
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
     pub const fn new() -> Self {
         Driver {
             driver: riot_sys::saul_driver_t {
-                read: if D::HAS_READ {
+                read: if DRIV::HAS_READ {
                     Some(Self::read_raw)
                 } else {
                     Some(riot_sys::saul_notsup)
                 },
-                write: if D::HAS_WRITE {
+                write: if DRIV::HAS_WRITE {
                     Some(Self::write_raw)
                 } else {
                     Some(riot_sys::saul_notsup)
                 },
-                type_: D::CLASS.to_c(),
+                type_: DRIV::CLASS.to_c(),
             },
             _phantom: core::marker::PhantomData,
         }
     }
 
     unsafe extern "C" fn read_raw(dev: *const libc::c_void, res: *mut riot_sys::phydat_t) -> i32 {
-        let device = &*(dev as *const D);
+        let device = &*(dev as *const DEV);
+        let device = device.into();
         match device.read() {
             Ok(d) => {
                 res.write(d.values);
@@ -100,7 +118,8 @@ impl<D: Drivable> Driver<D> {
     }
 
     unsafe extern "C" fn write_raw(dev: *const libc::c_void, data: *mut riot_sys::phydat_t) -> i32 {
-        let device = &*(dev as *const D);
+        let device = &*(dev as *const DEV);
+        let device = device.into();
         let data = *data;
         // PHYDAT_DIM: See write documentation
         let data = Phydat {
@@ -116,26 +135,48 @@ impl<D: Drivable> Driver<D> {
 }
 
 // unsafe: All the content we have in the inner struct is Send (being just plain functions), so is
-// the whole
-unsafe impl<D: Drivable> Send for Driver<D> {}
-
-pub struct Registration<D: Drivable> {
-    reg: riot_sys::saul_reg_t,
-    _phantom: core::marker::PhantomData<&'static D>,
+// the whole. (We don't store the DEV pointer here, so we don't need DEV Sync, but Driver needs it)
+unsafe impl<DEV, DRIV> Send for Driver<DEV, DRIV>
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
 }
 
-// unsafe: The registration is as Send as the pointer to the Drivable it contains (and Drivable
-// requires Sync)
-unsafe impl<'a, D: Drivable> Send for Registration<D> {}
+pub struct Registration<DEV, DRIV = &'static DEV>
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
+    reg: riot_sys::saul_reg_t,
+    _phantom: core::marker::PhantomData<(&'static DEV, DRIV)>,
+}
 
-impl<D: Drivable> Registration<D> {
+// unsafe: The registration is as Send as the pointer to the DEV it contains (and for DEV Sync is
+// required)
+unsafe impl<DEV, DRIV> Send for Registration<DEV, DRIV>
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
+}
+
+impl<DEV, DRIV> Registration<DEV, DRIV>
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
     // Unlike in the old implementation, no attempt is made to build it short-lived and then
     // upgrade -- not for lifetime reasons, but because for `register_static` all is already
     // static anyway, and `build_with` can just as well be called with the components, claim their
     // lifetime first and then go through here.
     pub fn new(
-        driver: &'static Driver<D>,
-        device: &'static D,
+        driver: &'static Driver<DEV, DRIV>,
+        device: &'static DEV,
         name: Option<&'static CStr>,
     ) -> Self {
         Registration {
@@ -169,12 +210,17 @@ impl<D: Drivable> Registration<D> {
 //
 // If unwinding is ever added in RIOT, this will need a guard similar to the one in the
 // `replace_with` crate.
-pub fn register_and_then<D: Drivable>(
-    driver: &Driver<D>,
-    device: &D,
+pub fn register_and_then<DEV, DRIV>(
+    driver: &Driver<DEV, DRIV>,
+    device: &DEV,
     name: Option<&CStr>,
     f: impl FnOnce() -> Never,
-) -> ! {
+) -> !
+where
+    DEV: Sized + Sync + 'static,
+    &'static DEV: Into<DRIV>,
+    DRIV: Drivable + 'static,
+{
     // Reborrow for 'static lifetime.
     //
     // This is safe because we never terminate, and thus keep these references forever.
@@ -182,7 +228,7 @@ pub fn register_and_then<D: Drivable>(
     // this anyway).
     let (driver, device, name) = unsafe { core::mem::transmute((driver, device, name)) };
 
-    let mut registration = Registration::<D>::new(driver, device, name);
+    let mut registration = Registration::<DEV, DRIV>::new(driver, device, name);
 
     (unsafe { riot_sys::saul_reg_add(&mut registration.reg) })
         .negative_to_error()
