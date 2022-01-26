edeb99a616eac71a4370cc63e1da9d76a115527a
chrysn <chrysn@fsfe.org>
2022-01-26 18:06:45 +0100

    build: Drop old version recognition
    Instead of comparing versions, concrete markers are checked now.

--- build.rs
+++ build.rs
@@ -9,10 +9,6 @@ fn main() {
 
     println!("cargo:rerun-if-env-changed=DEP_RIOT_SYS_CFLAGS");
 
-    /*
-    let mut riot_version_count = 0;
-    */
-
     for flag in cflags.iter() {
         if flag.starts_with("-DMODULE_") {
             // Some modules like cmsis-dsp_StatisticsFunctions have funny characters
@@ -36,25 +32,6 @@ fn main() {
         if flag == "-DDEVELHELP" {
             println!("cargo:rustc-cfg=riot_develhelp");
         }
-
-        /*
-        if flag.starts_with("-DRIOT_VERSION=") {
-            let tail = &flag[15..];
-            let uptodash = tail
-                .split(|x| x == '-')
-                .next()
-                .expect("Failed to parse RIOT_VERSION"); // Ignoring anything behind the dash
-            let numeric: Vec<u32> = uptodash
-                .split(|x| x == '.')
-                .map(|x| x.parse())
-                .collect::<Result<_, _>>()
-                .expect("Failed to parse RIOT_VERSION");
-            if numeric < vec![2019, 10] {
-                println!("cargo:rustc-cfg=riot_version_pre2019_10");
-            }
-            riot_version_count += 1;
-        }
-        */
     }
 
     for (key, _) in env::vars() {
@@ -63,10 +40,4 @@ fn main() {
             println!("cargo:rustc-cfg=marker_{}", marker);
         }
     }
-
-    /*
-    if riot_version_count != 1 {
-        panic!("RIOT_VERSION missing from the defines.");
-    }
-    */
 }
