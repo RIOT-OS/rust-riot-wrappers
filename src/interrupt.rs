1674987cb95af99aee6eb4e840cc501f7cce29d2
chrysn <chrysn@fsfe.org>
2022-10-05 11:09:30 +0200

    interrupt: deprecate the interrupt macro
    There are currently no drivers written that test this, and anyway it'd
    be highly platform specific. Let's do it right when there is need for
    it.

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,6 +1,6 @@
 //! Interaction with interrupts
 //!
-//! The RIOT wrappers offer three ways to interact with interrupts:
+//! The RIOT wrappers offer two ways to interact with interrupts:
 //!
 //! * Utility functions can disable interrupts (creating critical sections), check whether
 //!   interrupts are enabled or to determine whether code is executed in a thread or an ISR.
@@ -10,14 +10,11 @@
 //!
 //!   These are typechecked to be Send, as they are moved from the thread to the interrupt context.
 //!
-//! * Writing interrupt handlers (using the [`interrupt!`] macro).
-//!
-//!   Writing interrupt handlers is something that obviously needs some care; when using this module,
-//!   you must understand the implications of not doing so within the CPU implementation. (Note: The
-//!   author does not).
-//!
-//!   This is intended to be used for implementing special interfaces that have no generalization in
-//!   RIOT (eg. setting actions at particular points in a PWM cycle).
+//! Not provided by riot-wrappers are methods of implementing interrupts that are directly called
+//! by the CPU's interrupt mechanism. These are `extern "C"` functions (often with a `() -> ()`
+//! signature) exported under a particular name using `#[no_mangle]`. Any platform specifics (such
+//! as the [`riot_sys::inline::cortexm_isr_end()`] function) need to be managed by the
+//! implementer, just as when implementing a C interrupt.
 //!
 //! Rust code intended for use within interrupts does not generally need special precautions -- but
 //! several functions (generally, anything that blocks) are discouraged (as they may fail or stall
@@ -69,9 +66,14 @@ pub fn free<R, F: FnOnce(&CriticalSection) -> R>(f: F) -> R {
 
 /// Wrap a Rust interrupt handler in an extern "C" wrapper that does the post-return cleaups.
 ///
-/// This is probably Coretex-M specific.
+/// As with all code executed in interrupt contexts, the wrapped function should not panic.
 ///
-/// The wrapped function should not panic; FIXME: Explore the use of rustig to ensure this.
+/// ## Caveats
+///
+/// This is Cortex-M specific.
+#[deprecated(
+    note = "See module documentation: This needs to be done manually per platform; it is incomplete as riot-wrappers provides no method of enabling platform specific interrupts, and provides no other access to configure the peripheral through registers. If it is re-introduced, it will likely carry an `InIrq` token into the function."
+)]
 #[macro_export]
 macro_rules! interrupt {
     ($isr_name:ident, $rust_handler:expr) => {
@@ -79,10 +81,7 @@ macro_rules! interrupt {
         pub extern "C" fn $isr_name() -> () {
             $rust_handler();
 
-            // EXPANDED cpu/cortexm_common/include/cpu.h:189 (cortexm_isr_end)
-            if unsafe { core::ptr::read_volatile(&riot_sys::sched_context_switch_request) } != 0 {
-                unsafe { riot_sys::thread_yield_higher() };
-            }
+            unsafe { riot_sys::inline::cortexm_isr_end() };
         }
     };
 }
