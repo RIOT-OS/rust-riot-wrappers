b6eada927c77089ff597d62a27b98a85d20725e8
Kilian Barning <barning@uni-bremen.de>
2023-03-21 20:16:52 +0100

    Make internal construct fn unsafe

--- src/uart.rs
+++ src/uart.rs
@@ -99,7 +99,8 @@ pub struct UartDevice<'scope> {
 }
 
 impl<'scope> UartDevice<'scope> {
-    fn construct_uart<F>(
+    /// Unsafety: To use this safely, the caller must ensure that the returned Self is destructed before &'scope mut F becomes unavailable.
+    unsafe fn construct_uart<F>(
         index: usize,
         baud: u32,
         user_callback: &'scope mut F,
@@ -107,26 +108,24 @@ impl<'scope> UartDevice<'scope> {
     where
         F: FnMut(u8) + Send + 'scope,
     {
-        unsafe {
-            let dev = macro_UART_DEV(index as c_uint);
-            uart_init(
-                dev,
-                baud,
-                Some(Self::new_data_callback::<F>),
-                user_callback as *mut _ as *mut c_void,
-            )
-            .negative_to_error()
-            .map(|_| Self {
-                dev,
-                _scope: PhantomData,
-            })
-            .map_err(UartDeviceError::from_c)
-        }
+        let dev = macro_UART_DEV(index as c_uint);
+        uart_init(
+            dev,
+            baud,
+            Some(Self::new_data_callback::<F>),
+            user_callback as *mut _ as *mut c_void,
+        )
+        .negative_to_error()
+        .map(|_| Self {
+            dev,
+            _scope: PhantomData,
+        })
+        .map_err(UartDeviceError::from_c)
     }
 
 
-    /// Tries to initialize the given `UART`. Returns a Result with rather `Ok<Self>` if the UART was initialized successfully or a
-    /// `Err<UartDeviceStatus>` containing the error
+    /// Tries to initialize the given `UART`. Returns a Result with rather `Ok<RMain>` where `RMain` is the value returned by the scoped main function
+    /// or a `Err<UartDeviceStatus>` containing the error
     ///
     /// This is the scoped version of [`new()`] that can be used if you want to use short-lived callbacks, such as
     /// closures or anything containing references. The UartDevice is deconfigured when the internal main function
@@ -141,14 +140,14 @@ impl<'scope> UartDevice<'scope> {
     ///
     /// # Examples
     /// ```
-    ///use riot_wrappers::uart::UartDevice;
-    ///let mut cb = |new_data| {
+    /// use riot_wrappers::uart::UartDevice;
+    /// let mut cb = |new_data| {
     ///     //do something here with the received data
-    ///};
-    ///let mut scoped_main = |self_: &mut UartDevice| loop {
+    /// };
+    /// let mut scoped_main = |self_: &mut UartDevice| loop {
     ///    self_.write(b"Hello from UART")
-    ///};
-    ///let mut uart = UartDevice::new_scoped(0, 115200, &mut cb, scoped_main)
+    /// };
+    /// let mut uart = UartDevice::new_scoped(0, 115200, &mut cb, scoped_main)
     ///    .unwrap_or_else(|e| panic!("Error initializing UART: {e:?}"));
     /// ```
     pub fn new_scoped<F, Main, RMain>(
@@ -162,7 +161,7 @@ impl<'scope> UartDevice<'scope> {
         Main: FnOnce(&mut Self) -> RMain,
     {
         // This possibly relies on Rust code in RIOT to not unwind.
-        let mut self_ = Self::construct_uart(index, baud, user_callback)?;
+        let mut self_ = unsafe { Self::construct_uart(index, baud, user_callback) }?;
         let result = (main)(&mut self_);
         drop(self_);
         Ok(result)
@@ -375,6 +374,6 @@ impl UartDevice<'static> {
     where
         F: FnMut(u8) + Send + 'static,
     {
-        Self::construct_uart(index, baud, user_callback)
+        unsafe { Self::construct_uart(index, baud, user_callback) }
     }
 }
