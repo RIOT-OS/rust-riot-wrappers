748fe931e88564652632ba9d505e516885dd766d
Kilian Barning <barning@uni-bremen.de>
2023-03-21 20:33:48 +0100

    add test

---
+++ tests/uart/src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+use riot_wrappers::println;
+use riot_wrappers::riot_main;
+use riot_wrappers::uart;
+
+riot_main!(main);
+
+fn main() {
+    let mut cb = |new_data| {
+        //do something here with the received data
+    };
+    let mut scoped_main = |self_: &mut UartDevice| loop {
+        self_.write(b"Hello from UART")
+    };
+    let mut uart = UartDevice::new_scoped(0, 115200, &mut cb, scoped_main)
+        .unwrap_or_else(|e| panic!("Error initializing UART: {e:?}"));
+}
