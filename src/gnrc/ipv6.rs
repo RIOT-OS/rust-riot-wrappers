RIOT-OS/rust-riot-wrappers

commit 0ecf87e765f7264e79226540b694eb50993c7882

gnrc: Refactor Address to use an existing safe wrapper

src/gnrc/ipv6.rs
@@ -101,7 +101,7 @@ impl ::core::str::FromStr for Address {
 // When no_std_net / embedded_nal is present, it may be a good idea to run through there.
 impl ::core::fmt::Debug for Address {
     fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
-        let as_u8 = unsafe { &self.inner.u8_ };
+        let as_u8 = self.raw();
         write!(
             f,
             "{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}:\
@@ -180,7 +180,7 @@ impl From<embedded_nal::Ipv6Addr> for Address {
 #[cfg(feature = "with_embedded_nal")]
 impl From<Address> for embedded_nal::Ipv6Addr {
     fn from(addr: Address) -> Self {
-        Self::from(unsafe { addr.inner.u8_ })
+        Self::from(self.raw())
     }
 }
 

commit 220b7cd7b56bf2233d6c069ec0277282bdb4cc98

gnrc: Fix address conversion build failure
When building with_embedded_nal, a build regression was introduced in
0ecf87e765f7264e79226540b694eb50993c7882 through careless refactoring.
This restores buildability.

src/gnrc/ipv6.rs
@@ -180,7 +180,7 @@ impl From<embedded_nal::Ipv6Addr> for Address {
 #[cfg(feature = "with_embedded_nal")]
 impl From<Address> for embedded_nal::Ipv6Addr {
     fn from(addr: Address) -> Self {
-        Self::from(self.raw())
+        Self::from(*addr.raw())
     }
 }
 