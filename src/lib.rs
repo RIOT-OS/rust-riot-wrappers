9a56c59364241233e02825ca8683e5a9a236d4ae
chrysn <chrysn@fsfe.org>
2024-01-20 13:03:30 +0100

    coap: Gate by presence of relevant modules

--- src/lib.rs
+++ src/lib.rs
@@ -143,7 +143,9 @@ pub mod rwlock;
 #[cfg(feature = "set_panic_handler")]
 mod panic;
 
+#[cfg(riot_module_gcoap)]
 pub mod coap_handler;
+#[cfg(riot_module_gcoap)]
 pub mod coap_message;
 
 #[cfg(riot_module_sock)]
