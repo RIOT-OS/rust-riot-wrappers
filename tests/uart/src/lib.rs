61c039513f37049aadc4a6d70bbd12260c78c2bc
chrysn <chrysn@fsfe.org>
2024-12-09 11:01:00 +0100

    tests/uart: Address warnings

--- tests/uart/src/lib.rs
+++ tests/uart/src/lib.rs
@@ -1,18 +1,18 @@
 #![no_std]
 
-use riot_wrappers::println;
 use riot_wrappers::riot_main;
 use riot_wrappers::uart::UartDevice;
 
 riot_main!(main);
 
 fn main() {
+    let mut latest_byte = None;
     let mut cb = |new_data| {
-        //do something here with the received data
+        latest_byte = Some(new_data);
     };
-    let mut scoped_main = |self_: &mut UartDevice| loop {
+    let scoped_main = |self_: &mut UartDevice| loop {
         self_.write(b"Hello from UART")
     };
-    let mut uart = UartDevice::new_scoped(0, 115200, &mut cb, scoped_main)
+    UartDevice::new_scoped(0, 115200, &mut cb, scoped_main)
         .unwrap_or_else(|e| panic!("Error initializing UART: {e:?}"));
 }
